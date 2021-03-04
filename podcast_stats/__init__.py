fff7adfe6cd11a207039959f5d6c380eb41504b4
Stephen Brown II <Stephen.Brown2@gmail.com>
2021-03-04 16:10:31 -0700

    Updates

--- podcast_stats/__init__.py
+++ podcast_stats/__init__.py
@@ -1,20 +1,21 @@
 """podcast_stats - A script to pull the rss feed for a podcast and run some analysis on it."""
-import feedparser
-from datetime import datetime, timedelta
-from time import mktime
+from collections import Counter
 
-__version__ = '0.1.0'
-__author__ = 'Niklas Meinzer <github@niklas-meinzer.de>'
+import feedparser
+from pendulum import constants, duration, from_format, now, timezone
+
+__version__ = "0.1.0"
+__author__ = "Niklas Meinzer <github@niklas-meinzer.de>"
 __all__ = []
 
 
 def get_parsed_feed(feed_url):
     """
-        Pull a podcast rss feed from a given url and calculate
-        time between episodes.
+    Pull a podcast rss feed from a given url and calculate
+    time between episodes.
 
-        :raises ValueError:
-            If the feed does not have all of the required data
+    :raises ValueError:
+        If the feed does not have all of the required data
     """
 
     parsed_feed = feedparser.parse(feed_url)
@@ -25,40 +26,56 @@ def get_parsed_feed(feed_url):
     episodes = []
 
     for index, entry in enumerate(reversed(parsed_feed["entries"])):
-        struct_time = mktime(entry.published_parsed)
-        published_datetime = datetime.fromtimestamp(struct_time)
+        try:
+            published_datetime = from_format(entry.published, constants.RSS)
+        except ValueError:
+            naive, _, tz_abbr = entry.published.rpartition(" ")
+            tz_abbr = tz_abbr.replace("PST", "US/Pacific").replace("PDT", "US/Pacific")
+            published_datetime = from_format(
+                naive,
+                constants.RSS.removesuffix(" ZZ"),
+                tz=timezone(tz_abbr),
+            )
+
         if index == 0:
-            # use an empty timedelta for the first episode
-            time_since_last = timedelta()
+            # use an empty Duration for the first episode
+            time_since_last = duration()
         else:
             time_since_last = published_datetime - last_datetime
 
         episodes.append(
             {
+                "podcast": parsed_feed.feed.title,
                 "title": entry.title,
                 "published_datetime": published_datetime,
                 "time_since_last": time_since_last,
-                "published": True
+                "published": True,
             }
         )
         last_datetime = published_datetime
 
-    # Add a dummy episode to measure the time from the last episode until now
-    episodes.append({
-        "title": "[Next unpublished episode]",
-        "published_datetime": datetime.now(),
-        "time_since_last": datetime.now() - last_datetime,
-        "published": False
-    })
+    average = sum([e["time_since_last"].days for e in episodes]) / len(
+        list(filter(lambda e: e["time_since_last"].days > 0, episodes))
+    )
+    # Add a dummy episode to measure the time from the last episode until the next
+    episodes.append(
+        {
+            "podcast": parsed_feed.feed.title,
+            "title": "[Next unpublished episode]",
+            "published_datetime": last_datetime + duration(days=average),
+            "time_since_last": now() - (last_datetime + duration(days=average)),
+            "published": False,
+        }
+    )
 
     return episodes
 
 
 def weekday_distribution(feed):
-    result = [0] * 7
+    result = Counter()
 
     for entry in feed:
         if entry["published"]:
-            result[entry["published_datetime"].weekday()] += 1
+            result[entry["published_datetime"].format("ddd")] += 1
 
     return result
--- podcast_stats/main.py
+++ podcast_stats/main.py
@@ -1,36 +1,73 @@
-from terminaltables import AsciiTable
 import click
+import pendulum
+from rich.color import Color
+from rich.console import Console
+from rich.style import Style
+from rich.table import Table
 
 from podcast_stats import get_parsed_feed, weekday_distribution
 
+
 @click.command()
-@click.option('--feed_url', prompt=True)
+@click.option("--feed-url", prompt=True, multiple=True)
 def run(feed_url):
     """
-        Pull a podcast rss feed from a given url and print a table
-        with all episodes sorted by time between episodes
+    Pull a podcast rss feed from a given url and print a table
+    with all episodes sorted by time between episodes
 
-        :raises ValueError:
-            If the feed does not have all of the required data
+    :raises ValueError:
+        If the feed does not have all of the required data
     """
+    console = Console(width=150)
     # get the feed and parse it
-    parsed_feed = get_parsed_feed(feed_url)
+    parsed_feed = []
+    for url in feed_url:
+        parsed_feed.extend(get_parsed_feed(url))
 
     # Draw weekday distribution
-    weekday_distr = weekday_distribution(parsed_feed)
-    weekday_table_data = [
-        ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"], weekday_distr
-    ]
-    table = AsciiTable(weekday_table_data, title="Weekday Heatmap")
-    print("\n")
-    print(table.table)
+    heatmap = Table(title="Weekday Heatmap")
+    heat = {
+        0: Style(bgcolor=Color.from_rgb(red=153, green=0, blue=13)),
+        1: Style(bgcolor=Color.from_rgb(red=203, green=24, blue=29)),
+        2: Style(bgcolor=Color.from_rgb(red=239, green=59, blue=44)),
+        3: Style(bgcolor=Color.from_rgb(red=251, green=106, blue=74), color="black"),
+        4: Style(bgcolor=Color.from_rgb(red=252, green=146, blue=114), color="black"),
+        5: Style(bgcolor=Color.from_rgb(red=252, green=187, blue=161), color="black"),
+        6: Style(bgcolor=Color.from_rgb(red=254, green=229, blue=217), color="black"),
+    }
 
-    print("\n")
-    # create table data
-    table_data = [["Title", "Date published", "Days since last episode"]]
-    for episode in sorted(parsed_feed, key=lambda x: x["time_since_last"], reverse=True):
-        table_data.append([
-            episode["title"], episode["published_datetime"].isoformat(), episode["time_since_last"].days
-        ])
-    table = AsciiTable(table_data, title="Episodes by days since previous one")
-    print(table.table)
+    count = weekday_distribution(parsed_feed)
+    result = {}
+    for i, item in enumerate(count.most_common()):
+        result[item[0]] = {"value": str(item[1]), "style": heat[i]}
+
+    row = []
+    for d in ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]:
+        day = result.get(d, {"value": "0", "style": ""})
+        heatmap.add_column(d, style=day["style"], justify="center")
+        row.append(day["value"])
+
+    heatmap.add_row(*row)
+
+    console.print(heatmap)
+
+    table = Table(
+        "Podcast",
+        "Title",
+        "Date published",
+        "Days since last",
+        title="Episodes",
+    )
+    for episode in sorted(
+        parsed_feed, key=lambda x: x["published_datetime"], reverse=True
+    ):
+        table.add_row(
+            episode["podcast"],
+            " ".join(episode["title"].split()),
+            episode["published_datetime"]
+            .in_timezone(pendulum.local_timezone())
+            .to_day_datetime_string(),
+            str(episode["time_since_last"].days),
+        )
+
+    console.print(table)
